13b6151a96e2c3b451f1b3aa83ce07ac9dd97b55
SvetaIlina <139857261+SvetaIlina@users.noreply.github.com>
2024-05-05 23:10:27 +0400

    RSS-ECOMM-1 development environment configuration and scripts (#4)
    * feat: RSS-ECOMM-1_08 Set up and configure Vite
    
    * feat: RSS-ECOMM-1_09 Configure TypeScript
    
    * refactor: RSS-ECOMM-1_08 remove unnecessary .gitignore
    
    * feat: RSS-ECOMM-1_10 Set up ESLint
    
    * feat: RSS-ECOMM-1_14 Create a script for running ESLint
    
    * feat: RSS-ECOMM-1_11 Configure Prettier
    
    * feat:   RSS-ECOMM-1_15 Create a script for running Prettier
    
    * feat: RSS-ECOMM-1_12 Initialize Husky
    
    * feat:  RSS-ECOMM-1_13 Install and configure Jest
    
    * feat: RSS-ECOMM-1_16 Create a script for running Jest
    
    * fix: add right config for ignore Patterns
    
    * refactor: remove README
    
    * feat: RSS-ECOMM-1_13 add updating jest config and new package
    
    * feat: add new script for commits

---
+++ kickFlip-project/.eslintrc.cjs
@@ -0,0 +1,55 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb',
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['@typescript-eslint', 'react', 'prettier'],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.ts', '.js'],
+            },
+        },
+    },
+    ignorePatterns: ['*.config.cjs'],
+    rules: {
+        'react/react-in-jsx-scope': 0,
+        '@typescript-eslint/no-explicit-any': 2,
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+    },
+};
