64a413decddbb94f75761457ff3e737ef13fb9ef
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-05-15 23:52:35 +0300

    feat: RSS-ECOMM-2_08-16-21 Add navigation to login and registration pages for unauthorized users.

--- kickFlip-project/src/pages/login/loginPage.tsx
+++ kickFlip-project/src/pages/login/loginPage.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
 import LoginForm from '@/components/form/LoginForm/loginForm';
+import './loginPage.css';
 
 export default function LoginPage() {
-    return <LoginForm />;
+    return (
+        <>
+            <LoginForm />
+            <div className="login-question">
+                <p className="login-question-text">New to Kickplip?</p>
+            </div>
+            <Link className="login-register-button" to="/registration">
+                CREATE YOUR ACCOUNT
+            </Link>
+        </>
+    );
 }
--- kickFlip-project/src/pages/registration/registrationPage.tsx
+++ kickFlip-project/src/pages/registration/registrationPage.tsx
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom';
+
 import RegistrationForm from '@/components/form/registrationForm/regForm';
 import './regPage.css';
 
 export default function RegistrationPage() {
     return (
-        <div className="reg-page-wrapper ">
-            <h1 className="form-title">Registration</h1>
-            <RegistrationForm />
-        </div>
+        <>
+            <div className="reg-page-wrapper ">
+                <h1 className="form-title">Registration</h1>
+                <RegistrationForm />
+            </div>
+            <div className="reg-page-question">
+                <p className="reg-page-question-text">Have an account in Kickflip?</p>
+            </div>
+            <Link className="reg-page-login-button" to="/login">
+                LOGIN WITH YOUR ACCOUNT
+            </Link>
+        </>
     );
 }
