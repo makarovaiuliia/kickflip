27367df871a5702f53796c5c4df5de4b94e59a52
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-05-15 23:43:02 +0300

    feat: RSS-ECOMM-2_17-18-19 Add basic pages navigation. Add Basic Routing.

--- kickFlip-project/src/components/app/app.tsx
+++ kickFlip-project/src/components/app/app.tsx
@@ -1,7 +1,28 @@
-import RegistrationPage from '@/pages/registration/registrationPage';
+// import RegistrationPage from '@/pages/registration/registrationPage';
+
+import { Routes, Route } from 'react-router-dom';
+
+// import Header from '../header/header';
+import HomePage from '../../pages/home/homePage';
+import LoginPage from '../../pages/login/loginPage';
+import RegistrationPage from '../../pages/registration/registrationPage';
+import ProductsPage from '@/pages/products/productsPage';
+import CartPage from '../../pages/cart/cartPage';
+
+import Page from '../../pages/page';
 
 function App() {
-    return <RegistrationPage />;
+    return (
+        <Routes>
+            <Route path="/" element={<Page />}>
+                <Route index element={<HomePage />} />
+                <Route path="products" element={<ProductsPage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="registration" element={<RegistrationPage />} />
+                <Route path="cart" element={<CartPage />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
