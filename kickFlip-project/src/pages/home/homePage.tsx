0094749b1f28138e6f8f8a890baa0adcc1a3b9f5
SvetaIlina <139857261+SvetaIlina@users.noreply.github.com>
2024-05-20 11:48:29 +0400

    feat: RSS-ECOMM-2_11 displaying successful registration message
    * feat: add new user.state properties
    
    * feat: add success message  to the user upon successful account creation

--- kickFlip-project/src/pages/home/homePage.tsx
+++ kickFlip-project/src/pages/home/homePage.tsx
@@ -1,9 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './homePage.css';
+import { useEffect, useState } from 'react';
+import { clearRegistrationMessage, getUserSelector } from '@/services/userSlice';
 
 export default function HomePage(): JSX.Element {
+    const dispatch = useDispatch();
+    const { registrationMessage } = useSelector(getUserSelector);
+    const [showMessage, setShowMessage] = useState(false);
+
+    useEffect(() => {
+        let timer: NodeJS.Timeout;
+        if (registrationMessage) {
+            setShowMessage(true);
+            timer = setTimeout(() => {
+                setShowMessage(false);
+                setTimeout(() => {
+                    dispatch(clearRegistrationMessage());
+                }, 1000);
+            }, 5000);
+        }
+        return () => {
+            if (timer) clearTimeout(timer);
+        };
+    }, [registrationMessage, dispatch]);
+
     return (
         <div className="home-wrapper">
             <h1 className="home-title">Home page will be here</h1>
+            <p className={`successful-message ${showMessage ? 'show' : 'hide'}`}>{registrationMessage}</p>
         </div>
     );
 }
