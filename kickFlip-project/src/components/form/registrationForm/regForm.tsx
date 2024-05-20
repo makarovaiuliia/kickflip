a9bc2c791a0a143f32ecb83d74968cf429bcedc5
Pavel-Kuvshinov <94765270+Pavel-Kuvshinov@users.noreply.github.com>
2024-05-20 10:18:41 +0300

    fix: RSS-ECOMM-2_17-Add responsive styles for navigation. Add favicon. (#36)
    * merge Release/login registration main into branch userLoginUsingTokens
    
    * feat/RSS-ECOMM-2_12 implement error handling for failed registration attempts (#22)
    
    * style: change columns size
    
    * feat: add displaying error for failed registration attempts
    
    * fix: postal code error display when it is filled in before country
    
    * feat: RSS-ECOMM-2-08-16-17-18-19-20-21 Add page navigation with basic routing
    
    * feat: RSS-ECOMM-2_17-18-19 Add basic pages navigation. Add Basic Routing.
    
    * feat: RSS-ECOMM-2_20 Add "404" page.
    
    * feat: RSS-ECOMM-2_08-16-21 Add navigation to login and registration pages for unauthorized users.
    
    * fix: RSS-ECOMM-2_17-18-19 A separate "Header" component has been added. A "basicLayout" component has been added. Links to the "Log in" and "Cart" pages has been added. Some mistakes in styles has been fixed.
    
    * feat: refactor, style redirect promt, add NavLink tag
    
    * style: active links
    
    ---------
    
    Co-authored-by: Iuliia Makarova <iuliia.makarova.au@gmail.com>
    Co-authored-by: iuliia <144937274+makarovaiuliia@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SvetaIlina <139857261+SvetaIlina@users.noreply.github.com>
    Co-authored-by: Pavel-Kuvshinov <94765270+Pavel-Kuvshinov@users.noreply.github.com>
    
    * feat: add redirection after successful registration and login
    
    * fix: token in authorization headers
    
    * feat: isAuth dependancy in navLink
    
    * refactor: turn paths to aliases
    
    * feat: add protected routes
    
    * feat: add logout functionality
    
    * fix: bug with deleting tokens after logout
    
    * feat: add shipping and billing addresses to the customer profile
    
    * feat: add env file and remove eslint rule
    
    * refactor: add basic token from .env file
    
    * feat: RSS-ECOMM-2_17 Add responsive header styles.
    
    * feat: RSS-ECOMM-2_17_AddResponsiveHeader Add faviicon. Fix mistake in log out button click function.
    
    * fix: RSS-ECOMM-2_17_AddResponsiveHeader Add nessary rull for favicon in vite.config.ts.
    
    * fix: RSS-ECOMM-2_17_AddResponsiveHeader fix naming destinations. Fix some responsive styles in header.
    
    ---------
    
    Co-authored-by: iuliia <144937274+makarovaiuliia@users.noreply.github.com>
    Co-authored-by: SvetaIlina <139857261+SvetaIlina@users.noreply.github.com>
    Co-authored-by: Iuliia Makarova <iuliia.makarova.au@gmail.com>

--- kickFlip-project/src/components/form/registrationForm/regForm.tsx
+++ kickFlip-project/src/components/form/registrationForm/regForm.tsx
@@ -64,7 +64,7 @@ export default function RegistrationForm() {
         }
         return true;
     };
-
+  
     const watchShippingCountry = watch('shippingAddress.country');
     const watchBillingCountry = watch('billingAddress.country');
 
@@ -80,6 +80,11 @@ export default function RegistrationForm() {
         }
     }, [watchBillingCountry, trigger]);
 
+    useEffect(() => {
+        if (watchBillingCountry) {
+            trigger('billingAddress.postalCode');
+        }
+    }, [watchBillingCountry, trigger]);
     return (
         <form className="reg-form" onSubmit={handleSubmit(submit)}>
             <FormField
