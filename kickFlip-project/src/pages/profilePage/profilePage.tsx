5ff6736a90cdcb6b0410d86103fa2660097924e3
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-05-22 15:43:08 +0300

    feat: RSS-ECOMM-3 Add separated wrapper classes for main section.

--- kickFlip-project/src/pages/profilePage/profilePage.tsx
+++ kickFlip-project/src/pages/profilePage/profilePage.tsx
@@ -2,7 +2,7 @@ import './profilePage.css';
 
 export default function ProfilePage(): JSX.Element {
     return (
-        <div className="profile-wrapper">
+        <div className="main-wrapper profile-wrapper">
             <h1 className="home-title profile-page-title">My account</h1>
         </div>
     );
