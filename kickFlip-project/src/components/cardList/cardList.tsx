dea80dca648147ac0363ccb5e536f32ca920ec2c
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2024-05-24 16:20:50 +0200

    refactor: adjustments

--- kickFlip-project/src/components/cardList/cardList.tsx
+++ kickFlip-project/src/components/cardList/cardList.tsx
@@ -8,10 +8,13 @@ interface CardListProps {
 
 function CardList({ products }: CardListProps): JSX.Element {
     return (
-        <div className="card-list">
-            {products.map((product) => (
-                <Card productInfo={product} key={product.id} />
-            ))}
+        <div className="main-wrapper">
+            <div className="card-list">
+                {products.map((product) => (
+                    <Card productInfo={product} key={product.id} />
+                ))}
+            </div>
+            s
         </div>
     );
 }
--- kickFlip-project/src/components/header/header.tsx
+++ kickFlip-project/src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { NavLink, useNavigate, useMatch } from 'react-router-dom';
+import { NavLink, Link, useNavigate, useMatch } from 'react-router-dom';
 import { useDispatch, useSelector } from '@/services/store';
 
 /* eslint-disable import/no-absolute-path */
