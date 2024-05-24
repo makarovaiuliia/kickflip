f3300d64214a4ff7c8bea10a3205842ba08e857e
Svetlana <svetailina1987@gmail.com>
2024-05-24 15:51:44 +0400

    feat: add general component DetailsContainer for displaying products info

--- kickFlip-project/src/types/componentsInterfaces.ts
+++ kickFlip-project/src/types/componentsInterfaces.ts
@@ -31,3 +31,10 @@ export interface ProductDescriptionProps {
 export interface ProductSizesProps {
     sizes: number[];
 }
+
+export interface DetailsContainerProps {
+    infoProps: ProductInfoProps;
+    variantProps: VariantImagesProps;
+    sizesProps: ProductSizesProps;
+    descrProps: ProductDescriptionProps;
+}
