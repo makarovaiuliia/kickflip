7db4ba52adbb324a9a3cbf1be23fe16976adfced
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2024-05-31 13:31:00 +0200

    refactor: change layout in card list component

--- kickFlip-project/src/components/cardList/cardList.tsx
+++ kickFlip-project/src/components/cardList/cardList.tsx
@@ -28,25 +28,27 @@ function CardList({ products, setCategories, categories }: CardListProps): JSX.E
 
     return (
         <div className="card-list-container">
-            <div className="card-list-sorting-container">
+            <div className="card-list-info-container">
                 <h2 className="card-list-title">{`The best kicks (${products.length})`}</h2>
-                <form onSubmit={handleSearch}>
-                    <input
-                        type="text"
-                        className="card-list-sorting"
-                        placeholder="Search"
-                        onChange={(event) => setSearchTerm(event.target.value)}
-                        value={searchTerm}
-                    />
-                    <button type="submit" className="card-list-search-button" aria-label="Search" />
-                </form>
-                <select id="sort-select" className="card-list-sorting" onChange={handleSelect}>
-                    <option value="">Sort by</option>
-                    <option value="price asc">Price (Low-High)</option>
-                    <option value="price desc">Price (High-Low)</option>
-                    <option value="name.en-US desc">Name ⭡</option>
-                    <option value="name.en-US asc">Name ⭣</option>
-                </select>
+                <div className="card-list-sorting-container">
+                    <form onSubmit={handleSearch}>
+                        <input
+                            type="text"
+                            className="card-list-sorting"
+                            placeholder="Search"
+                            onChange={(event) => setSearchTerm(event.target.value)}
+                            value={searchTerm}
+                        />
+                        <button type="submit" className="card-list-search-button" aria-label="Search" />
+                    </form>
+                    <select id="sort-select" className="card-list-sorting" onChange={handleSelect}>
+                        <option value="">Sort by</option>
+                        <option value="price asc">Price (Low-High)</option>
+                        <option value="price desc">Price (High-Low)</option>
+                        <option value="name.en-US desc">Name ⭡</option>
+                        <option value="name.en-US asc">Name ⭣</option>
+                    </select>
+                </div>
             </div>
             {products.length === 0 ? (
                 <p className="card-list-title">No kicks match these filters. Try modifying your search</p>
