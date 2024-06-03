0407d902797998edfe66781f181a0e886d75f348
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2024-06-03 11:16:47 +0200

    fix: change sneakers to catalog

--- kickFlip-project/src/components/header/header.tsx
+++ kickFlip-project/src/components/header/header.tsx
@@ -59,7 +59,7 @@ export default function Header() {
                                 to="/products"
                                 onClick={closeMenu}
                             >
-                                Sneakers
+                                Catalog
                             </NavLink>
                         </div>
                     </nav>
