47549d534fca26d4c04dda8c05b632a65b9df1e9
Svetlana <svetailina1987@gmail.com>
2024-06-12 20:03:11 +0400

    feat: add remove button component

---
+++ kickFlip-project/src/components/cart/removeItemBtn/removeItemBtn.tsx
@@ -0,0 +1,9 @@
+import './removeItemBtn.css';
+
+interface RemoveItemBtnProps {
+    onclick: () => void;
+}
+
+export default function RemoveItemBtn({ onclick }: RemoveItemBtnProps) {
+    return <button type="button" aria-label="delete-item" className="remove-btn" onClick={onclick} />;
+}
