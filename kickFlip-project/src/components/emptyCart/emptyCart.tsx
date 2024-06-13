4d39126033059e80fb8be6e1d407227225ca8a54
Svetlana <svetailina1987@gmail.com>
2024-06-13 18:05:39 +0400

    feat: add EmptyCart

---
+++ kickFlip-project/src/components/emptyCart/emptyCart.tsx
@@ -0,0 +1,9 @@
+import './emptyCart.css';
+
+export default function EmptyCart() {
+    return (
+        <div className="empty-cart-wrapper">
+            <h1 className="empty-cart-title">ооо</h1>
+        </div>
+    );
+}
