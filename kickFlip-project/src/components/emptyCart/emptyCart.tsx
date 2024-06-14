f3c6aef12468ddceff0f4b4514c55755bf33f0ee
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2024-06-14 11:44:02 +0200

    style: new sryle for empty cart

--- kickFlip-project/src/components/emptyCart/emptyCart.tsx
+++ kickFlip-project/src/components/emptyCart/emptyCart.tsx
@@ -1,16 +1,23 @@
 import { Link } from 'react-router-dom';
 import './emptyCart.css';
 
+/* eslint-disable import/no-absolute-path */
+import cart from '/cart.svg';
+/* eslint-enable import/no-absolute-path */
+
 export default function EmptyCart() {
     return (
         <div className="empty-cart-wrapper">
+            <img src={cart} alt="cart" className="empty-cart_icon" />
             <h1 className="empty-cart-title">
-                Your cart is <span className="accent">empty</span>
+                Your cart is empty<span className="accent">.</span>
             </h1>
-            <p className="empty-cart-text">Looks like you haven’t added any kicks yet.</p>
-            <p className="back-link-text">Check what we’ve got for you...</p>
+            <div className="empty-cart_text-container">
+                <p>Looks like you haven’t added any kicks yet.</p>
+                <p>Check what we’ve got for you!</p>
+            </div>
             <Link to="/products" className="back-to-catalog-link">
-                Go back to shopping
+                Back to Shopping
             </Link>
         </div>
     );
